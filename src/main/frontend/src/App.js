12847389799f651515c1fda213e588d49728db14
david <david.mejia.sanchez@gmail.com>
2021-10-29 15:02:06 +0100

    Arreglando fallos

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,35 +1,36 @@
-import React, { Component } from "react";
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Dashboard from '../Dashboard/Dashboard'
+import Preferences from '../Preferences/Preferences'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
-class App extends Component{
-  state = {
-      product: []
+function App() {
+  const [state, setstate] = useState({ product: [] })
+
+  const getproduct = async () => {
+    const response = await fetch('/products')
+    const body = await response.json()
+    setstate({ product: body })
   }
 
-  async componentDidMount(){
-      const response = await fetch('/products');
-      const body = await response.json();
-      this.setState({product: body});
-  }
+  getproduct()
 
-  render(){
-      const {product} = this.state;
-      return (
-          <div className="App">
-              <header className="App-header">
-                  <img src={logo} className="App-logo" alt="logo" />
-                      <div className="App-intro">
-                          <h2>Products</h2>
-                          {product.map(product =>
-                      <div key={product.idproduct}>
-                      {product.description} {product.price} ({product.creationDate})
-                </div>
-              )}
-              </div>
-            </header>
-          </div>
-      );
-  }
+  const { product } = state
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <div className='App-intro'>
+          <h2>Products</h2>
+          {product.map((product) => (
+            <div key={product.idproduct}>
+              {product.description} {product.price} ({product.creationDate})
+            </div>
+          ))}
+        </div>
+      </header>
+    </div>
+  )
 }
-export default App;
+export default App
