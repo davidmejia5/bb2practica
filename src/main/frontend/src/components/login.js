7034b79d8df48b961d4f73a76b4dd8f415c4145b
david <david.mejia.sanchez@gmail.com>
2021-11-03 15:58:15 +0000

    avanzando en el login

--- src/main/frontend/src/components/login.js
+++ src/main/frontend/src/components/login.js
@@ -1,25 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 
-function login() {
+function Login() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState([])
+
   const handleSumit = (e) => {
     e.preventDefault()
-    console.log('hello world')
+    if (email && password) {
+      console.log('submit the form')
+    } else {
+      console.log('empty values')
+    }
   }
 
   return (
     <Form>
       <Form.Group className='mb-3' controlId='formBasicEmail'>
         <Form.Label>Email address</Form.Label>
-        <Form.Control type='email' placeholder='Enter email' />
+        <Form.Control
+          type='email'
+          placeholder='Enter email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <Form.Text className='text-muted'>
           We'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
       <Form.Group className='mb-3' controlId='formBasicPassword'>
         <Form.Label>Password</Form.Label>
-        <Form.Control type='password' placeholder='Password' />
+        <Form.Control
+          type='password'
+          placeholder='Password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </Form.Group>
       <Form.Group className='mb-3' controlId='formBasicCheckbox'>
         <Form.Check type='checkbox' label='Check me out' />
@@ -31,4 +49,4 @@ function login() {
   )
 }
 
-export default login
+export default Login
